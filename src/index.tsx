ebc62dcb8bf6137adbe08d668cba4cf5a6537f13
HYMPYT <vgenlevchuk2016@gmail.com>
2022-10-29 17:42:19 +0300

    Updated

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  
   <React.StrictMode>
     <BrowserRouter basename="/triplanner">
       <App />
